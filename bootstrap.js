896b28fb84c70becb96e8ff37cb63a92867edb67
Siddhartha Dugar <dugar.siddhartha@gmail.com>
2013-04-23 23:09:27 -0500

    Actually hide the What's New page

--- bootstrap.js
+++ bootstrap.js
@@ -1,10 +1,13 @@
 Components.utils.import("resource://gre/modules/Services.jsm");
 
-function startup(data, reason) {}
+function startup(data, reason) {
+  Services.prefs.setCharPref("browser.startup.homepage_override.mstone","ignore");
+}
 
 function shutdown(data, reason) {
   /* Clear the value of lastAppVersion before every shutdown */
   Services.prefs.clearUserPref("extensions.lastAppVersion");
+  Services.prefs.setCharPref("browser.startup.homepage_override.mstone","");
 }
 
 function install(data, reason) {}
